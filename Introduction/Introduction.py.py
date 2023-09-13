5762e828f38b1d22e9f20d4474998bdda720b51a
fnpjups <144931469+fnpjups@users.noreply.github.com>
2023-09-13 19:21:08 +0400

    Add files via upload

---
+++ Introduction/Introduction.py.py
@@ -0,0 +1,25 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+import time
+
+print ("Hello everyone!")
+input = input("My name is ")
+if input=="Julianne Febrero":
+    print ("but you can call me Juls.", end="\n\n")
+
+time.sleep(1)
+
+print ("My birthday is February 10,2007 which makes me 16 years old as of today :>", end="\n\n")
+
+time.sleep(2)
+
+print ("My hobbies include")
+print ("PLaying guitar")
+print ("PLaying games")
+print ("and sleeping")
