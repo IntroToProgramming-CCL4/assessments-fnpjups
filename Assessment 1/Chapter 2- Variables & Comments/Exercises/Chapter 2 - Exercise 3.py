ee7854460dd6a9b3d6780d730456ba38bd05364a
fnpjups <144931469+fnpjups@users.noreply.github.com>
2023-11-09 13:57:19 +0400

    Add files via upload
    My submission for
    Exercise 3: Stripping Names
    Tidy up the code to make it easier to understand
    
    Use a variable to represent a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, “\t” and “\n”, at least once.
    
    Print the name once, so the whitespace around the name is displayed.
    
    Then print the name using each of the three stripping functions, lstrip(), rstrip(), and strip().

---
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/Chapter 2 - Exercise 3.py
@@ -0,0 +1,8 @@
+name ="\t\tJups\t\n"
+lstrip = name.lstrip()
+rstrip = name.rstrip()
+strip = name.strip()
+print(name) #Name with white spaces
+print(lstrip) #Name without left side whitespace
+print(rstrip) #Name without right side whitespace
+print(strip) #Name fully stripped
