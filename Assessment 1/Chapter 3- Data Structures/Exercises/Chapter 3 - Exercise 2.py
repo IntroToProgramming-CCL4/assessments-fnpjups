931a8adb745d4d7d1be724ef20ead76487f701ab
fnpjups <144931469+fnpjups@users.noreply.github.com>
2023-11-10 11:56:30 +0400

    Add files via upload
    My code for 
    Exercise 2: Greetings 
    Start with the list you used in Exercise 1, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

---
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Chapter 3 - Exercise 2.py
@@ -0,0 +1,3 @@
+names = ["Paul", "Rox", "Mir", "Venice", "Yumi", "Zahra", "Ifrah", "Khale", "Diane"]
+for n in names: #This is so that the code loops until the last element.
+    print("Hello", n + "!\n") #For every name in the list, it will print this message for them
