22c21238c8600f260f063268452a87f2174fcad8
fnpjups <144931469+fnpjups@users.noreply.github.com>
2023-11-10 11:58:24 +0400

    Add files via upload
    My code submission for 
    Exercise 4: Guest List 
    If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.

---
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Chapter 3 - Exercise 4.py
@@ -0,0 +1,4 @@
+name = ["Russell", "Rey", "Caitlyn", "Francis", "Beryl",] #Names of people to invite
+#Loop to send out invites to the people in the list
+for n in name:
+    print("Dear", n, "\nI would like to invite you to dinner on Nov. 23, 2023 at 6:00 PM. \nWe will be eating at my home therefore you are free to choose what you want to wear. \nPlease let me know soon if you would be able to attend. Thank you!\n")
