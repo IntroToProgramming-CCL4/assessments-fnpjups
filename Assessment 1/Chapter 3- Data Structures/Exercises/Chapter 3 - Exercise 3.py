4f21c78ed0bceff18b094cc5bc8e50ad1072897e
fnpjups <144931469+fnpjups@users.noreply.github.com>
2023-11-10 11:57:26 +0400

    Add files via upload
    Code for 
    Exercise 3: Your Own List 
    Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”

---
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Chapter 3 - Exercise 3.py
@@ -0,0 +1,6 @@
+transpo = ["Car", "Planes", "Train", "Bus"]#List of my favorite transportation
+#Printing statements about the items in the list
+print("One day I want to drive my own", transpo[0],) #Calling each element in the list starting from 0
+print("I find", transpo[1], "fascinating.")
+print("I think riding the", transpo[2], "is very efficient.")
+print("It would be fun to ride a", transpo[3], "around Dubai.")
