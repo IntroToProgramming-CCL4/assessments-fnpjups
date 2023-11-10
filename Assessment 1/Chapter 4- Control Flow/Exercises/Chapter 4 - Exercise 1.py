a8b87af47034c2e4a6bf9df6fadc7380f39802a3
fnpjups <144931469+fnpjups@users.noreply.github.com>
2023-11-10 12:11:11 +0400

    Add files via upload
    First submission for chapter 4 
    Exercise 1: Alien Colors #1
    Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
    
    •Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
    
    •Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)

---
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Chapter 4 - Exercise 1.py
@@ -0,0 +1,8 @@
+#Pass version
+alien_color = 'green'
+if alien_color == 'green':
+    print("Congrats, you earned 5 points!\n") #Prints because the statement is true
+#Fail version
+alien_color = 'yellow'
+if  alien_color == 'green':
+    print("Congrats, you earned 5 points!") #No output as the statement is false
