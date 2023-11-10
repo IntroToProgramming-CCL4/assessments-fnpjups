7db4ff68be94c13685148b682b49e7c4bfbc848d
fnpjups <144931469+fnpjups@users.noreply.github.com>
2023-11-10 12:41:36 +0400

    Add files via upload
    My code for 
    Exercise 2: Glossary 
    A Python dictionary can be used to model an actual dictionary. However, to avoid confusion, let’s call it a glossary.
    
    Think of five programming words you’ve learned about in the previous chapters. Use these words as the keys in your glossary, and store
    their meanings as values.
    
    Print each word and its meaning as neatly formatted output. You might print the word followed by a colon and then its meaning, or print
    the word on one line and then print its meaning indented on a second line. Use the newline character (\n) to insert a blank line between each word-meaning pair in your output.

---
+++ Assessment 1/Chapter 5- Dictionaries/Exercises/Chapter 5 - Exercise 2.py
@@ -0,0 +1,17 @@
+glossary = {'Infinite loops':'An infinite loop is a loop that never ends.',
+            'Variables':'Containers for storing data values.',
+            'Constants':'These are variable whose values do not change.',
+            'Operators':'Special symbols in python that perform arithmetic or logical computation.',
+            'Comments':'These describe what is going on inside a program.'} #Contents inside the glossary
+
+#Printing the Glossary
+key = 'Infinite loops'
+print("\n", key+":", glossary[key])
+key = 'Variables'
+print("\n", key+":", glossary[key])
+key = 'Constants'
+print("\n", key+":", glossary[key])
+key = 'Operators'
+print("\n", key+":", glossary[key])
+key = 'Comments'
+print("\n", key+":", glossary[key])
