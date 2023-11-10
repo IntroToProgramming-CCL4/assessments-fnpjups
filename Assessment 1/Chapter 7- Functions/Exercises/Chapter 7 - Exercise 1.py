7e19f77659d380021f1a57e9a11c7e8a488c3e6d
fnpjups <144931469+fnpjups@users.noreply.github.com>
2023-11-10 13:15:07 +0400

    Add files via upload
    My first submission for chapter 7 
    Exercise 1: Message ☑️
    Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. Call the function, and make sure the message displays correctly.

---
+++ Assessment 1/Chapter 7- Functions/Exercises/Chapter 7 - Exercise 1.py
@@ -0,0 +1,5 @@
+def display_message():
+    message = "I am learning how to code functions!" #What I want to print when I call on the function
+    print(message) #Code stored inside the function
+
+display_message() #Calling on the function
