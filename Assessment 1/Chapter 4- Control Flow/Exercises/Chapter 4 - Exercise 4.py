ff4dd7e614397215b1e95b651ca8b8e1710209bd
fnpjups <144931469+fnpjups@users.noreply.github.com>
2023-11-10 12:15:14 +0400

    Add files via upload
    My code for 
    Exercise 4: Stages of Life
    Write an if-elif-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
    
    •If the person is less than 2 years old, print a message that the person is a baby.
    
    •If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
    
    •If the person is at least 4 years old but less than 13, print a message that the person is a kid.
    
    •If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
    
    •If the person is at least 20 years old but less than 65, print a message that the person is an adult.
    
    •If the person is age 65 or older, print a message that the person is an elder.

---
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Chapter 4 - Exercise 4.py
@@ -0,0 +1,13 @@
+age = 16 #Set value
+if age < 2:
+    print("You are a baby :0") #Prints if they are less than 2
+elif age < 4:
+    print("You are a toddler") #Prints if input is less than 4
+elif age < 13:
+    print("You are a kid") #Prints if age is less than 13
+elif age < 20:
+    print("You're a teenager, just like me :D") #The code should print this output
+elif age < 65:
+    print("You are an adult")  #Prints when age is less than 65
+else:
+    print("You're an elder") #Prints if no conditions were met
